c656f78422505b10e4eda508dd9f57bb9f690521
fenota-viktoriya <89807001+fenota-viktoriya@users.noreply.github.com>
2022-06-20 01:02:48 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,29 +1,37 @@
-import { PureComponent } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Overlay, Modal } from './Modal.styled';
+import { createPortal } from 'react-dom';
 
-export class Modals extends PureComponent {
-  componentDidMount() {
-    const { closeModal } = this.props;
+const modalRoot = document.querySelector('#modal-root');
+
+export function Modals({ img, tags, onClose }) {
+  useEffect(() => {
+    const closeModal = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+
     window.addEventListener('keydown', closeModal);
     document.body.style.overflow = 'hidden';
-  }
-  componentWillUnmount() {
-    const { closeModal } = this.props;
-    window.removeEventListener('keydown', closeModal);
-    document.body.style.overflow = ' visible';
-  }
-  render() {
-    const { tags, img, closeModal } = this.props;
 
-    return (
-      <Overlay onClick={closeModal}>
-        <Modal src={`${img}`} alt={`${tags}`} />
-      </Overlay>
-    );
-  }
+    return () => {
+      window.removeEventListener('keydown', closeModal);
+      document.body.style.overflow = ' visible';
+    };
+  }, [onClose]);
+
+  return createPortal(
+    <Overlay onClick={onClose}>
+      <Modal src={`${img}`} alt={`${tags}`} />
+    </Overlay>,
+    modalRoot
+  );
 }
 
 Modals.propTypes = {
-  closeModal: PropTypes.func.isRequired,
+  img: PropTypes.string.isRequired,
+  tags: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired,
 };
